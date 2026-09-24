86a3def658d09426e3d1c678ac0cc8852c8b7dd3
KevinHbrck <kevin.habrock@gmail.com>
2026-09-24 10:27:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
  * und ohne Verbindung startet die zuletzt geladene aus dem Speicher.
  */
 
-var FASSUNG = "2026-09-24-5";
+var FASSUNG = "2026-09-24-6";
 var SPEICHER = "vokabelkasten-" + FASSUNG;
 var GRUNDGERUEST = ["./", "./index.html", "./manifest.json", "./icon.png"];
 
